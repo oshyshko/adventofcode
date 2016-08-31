d8ecf3380d4062765c1a9171bb00e4bd0de0e273
Oleksandr Shyshko <oshyshko@gmail.com>
2016-08-31 03:25:02 -0700

    Cleanup "run.sh"

--- run.sh
+++ run.sh
@@ -7,8 +7,10 @@ cd "$(dirname $0)"
 
 cd src
 
+# TODO loop over *.hs
+
 echo "## Running Day01.hs..."
-cat Day01.txt | stack runghc Day01.hs
+stack runghc Day01.hs
 
 echo "## Running Day02.hs..."
-cat Day02.txt | stack runghc Day02.hs
+stack runghc Day02.hs
