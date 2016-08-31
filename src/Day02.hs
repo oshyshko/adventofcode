b6f5c7cbf4cbb2532e47e8884923c559fa13792f
Oleksandr Shyshko <oshyshko@gmail.com>
2016-08-31 03:26:33 -0700

    Optimize imports for Day02

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,6 @@
 module Day02 where
 
-import Data.List.Split
+import Data.List.Split (splitOn)
 
 solve1 :: [[Int]] -> Int
 solve1 = sum . map (\ [w,h,l] -> let a = w * h
@@ -13,7 +13,7 @@ solve2 = sum . map (\ [w,h,l] -> (w*h*l) + 2 * minimum [w+h, h+l, w+l])
 
 main :: IO ()
 main = do
-  let lineTo3ints = map read . splitOn "x"
-  xs <- map lineTo3ints . lines <$> readFile "Day02.txt"
+  let lineTo3Ints = map read . splitOn "x"
+  xs <- map lineTo3Ints . lines <$> readFile "Day02.txt"
   print $ solve1 xs
   print $ solve2 xs
