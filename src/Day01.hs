4f2c03b6cb3e3e954143a7212626534af2cdd4ed
Oleksandr Shyshko <oshyshko@gmail.com>
2016-08-31 03:35:04 -0700

    Eta reduce Day01

--- src/Day01.hs
+++ src/Day01.hs
@@ -9,11 +9,12 @@ solve1 :: String -> Int
 solve1 = sum . map char2value
 
 solve2 :: String -> Int
-solve2 xs = fst . head
-  $ dropWhile (\(_, x) -> x >= 0)
-  $ zip [1..]
-  $ scanl (+) 0
-  $ map char2value xs
+solve2 = fst
+       . head
+       . dropWhile (\(_, x) -> x >= 0)
+       . zip [1..]
+       . scanl (+) 0
+       . map char2value
 
 main :: IO ()
 main = do
