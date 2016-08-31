6861b4dd71309ac5d5dd3c8ff4bf68faaa96a957
Oleksandr Shyshko <oshyshko@gmail.com>
2016-08-31 11:16:12 -0700

    Refactor Day01

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,20 +1,20 @@
 module Day01 where
 
-char2value :: Char -> Int
-char2value x = case x of '(' ->  1
-                         ')' -> -1
-                         _   ->  0
+char2move :: Char -> Int
+char2move x = case x of '(' ->  1
+                        ')' -> -1
+                        _   ->  error $ "Unexpected character: " ++ [x]
 
 solve1 :: String -> Int
-solve1 = sum . map char2value
+solve1 = sum . map char2move
 
 solve2 :: String -> Int
 solve2 = fst
        . head
-       . dropWhile (\(_, x) -> x >= 0)
+       . dropWhile (\ (_, x) -> x >= 0 )
        . zip [1..]
        . scanl (+) 0
-       . map char2value
+       . map char2move
 
 main :: IO ()
 main = do
