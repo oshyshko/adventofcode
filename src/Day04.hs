6b2362d08906d6d98b1781ed42562520645eda48
Oleksandr Shyshko <oshyshko@gmail.com>
2016-09-08 01:18:47 -0700

    Refactor Day04

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,33 +2,18 @@ module Day04 where
 
 import Data.ByteString.Lazy.Char8 (pack)
 import Data.Digest.Pure.MD5       (md5)
+import Data.List                  (isPrefixOf)
 import Util                       (juxt)
 
-startsWith5Zeros :: String -> Bool
-startsWith5Zeros ('0':'0':'0':'0':'0':_) = True
-startsWith5Zeros _                       = False
-
-startsWith6Zeros :: String -> Bool
-startsWith6Zeros ('0':'0':'0':'0':'0':'0':_) = True
-startsWith6Zeros _                           = False
-
-solve1 :: String -> Int
-solve1 s = fst
-         . head
-         . dropWhile (not . startsWith5Zeros . snd)
-         . map (\x -> (x, show . md5 . pack $ s ++ show x))
-         $ [1..]
-
--- TODO optimize + merge with solve1
-solve2 :: String -> Int
-solve2 s = fst
-         . head
-         . dropWhile (not . startsWith6Zeros . snd)
-         . map (\x -> (x, show . md5 . pack $ s ++ show x))
-         $ [1..]
+solve :: Int -> String -> Int
+solve n s = fst
+          . head
+          . filter (isPrefixOf (replicate n '0') . snd)
+          . map (\x -> (x, show . md5 . pack $ s ++ show x))
+          $ [1..]
 
 main :: IO ()
 main = do
   s <- readFile "Day04.txt"
-  let _ = juxt [solve1, solve2] s
+  -- print . juxt [solve 5, solve 6] $ s
   putStrLn "[117946,3938038] <-- replaced with a constant to save time"
