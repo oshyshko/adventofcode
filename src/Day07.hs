ae168d99ab4208bd3f56418dae49d36f18bd20ea
Oleksandr Shyshko <oshyshko@gmail.com>
2016-10-11 02:24:09 -0700

    Add Day07

---
+++ src/Day07.hs
@@ -0,0 +1,83 @@
+module Day07 where
+
+import           Control.Monad.State           (State, evalState, gets, modify)
+import           Data.Bits                     (complement, shiftL, shiftR,
+                                                (.&.), (.|.))
+import qualified Data.Map.Strict               as M
+import           Data.Word                     (Word16)
+
+import           Text.ParserCombinators.Parsec (GenParser, ParseError, digit,
+                                                endBy, letter, many1, parse,
+                                                string, try, (<|>))
+
+type RefId = String
+type Def = (RefId, Exp)
+
+data Exp = Val Word16
+         | Ref RefId
+         | Not Exp
+         | And Exp Exp
+         | Or  Exp Exp
+         | Lsh Exp Exp
+         | Rsh Exp Exp deriving Show
+
+parseDefs :: String -> Either ParseError [Def]
+parseDefs = parse defs ""
+  where
+    defs :: GenParser Char st [Def]
+    defs = def `endBy` eol
+
+    def :: GenParser Char st Def
+    def = do
+      left <- ex
+      string " -> "
+      right <- rv
+      case right of Ref refId -> return (refId, left)
+                    _         -> error $ "Expected Ref, but got: " ++ show right
+
+    rv :: GenParser Char st Exp
+    rv = Val . read <$> many1 digit
+            <|> Ref <$> many1 letter
+
+    ex :: GenParser Char st Exp
+    ex =  try (do          string "NOT ";     x <- rv; return $ Not x)
+      <|> try (do x <- rv; string " OR ";     y <- rv; return $ Or  x y)
+      <|> try (do x <- rv; string " AND ";    y <- rv; return $ And x y)
+      <|> try (do x <- rv; string " LSHIFT "; n <- rv; return $ Lsh x n)
+      <|> try (do x <- rv; string " RSHIFT "; n <- rv; return $ Rsh x n)
+      <|>      rv
+
+    eol :: GenParser Char st String
+    eol = try (string "\n\r")
+      <|> try (string "\r\n")
+      <|>      string "\n"
+      <|>      string "\r"
+
+eval :: Exp -> State (M.Map RefId Exp) Word16
+eval e = case e of Val v   -> return v
+
+                   Ref r   -> do ee <- gets (M.! r)
+                                 v <- eval ee
+                                 modify $ M.insert r (Val v)
+                                 return v
+
+                   Not x   -> complement <$> eval x
+                   And x y -> (.&.)  <$> eval x <*> eval y
+                   Or  x y -> (.|.)  <$> eval x <*> eval y
+                   Lsh x n -> shiftL <$> eval x <*> (fromIntegral <$> eval n)
+                   Rsh x n -> shiftR <$> eval x <*> (fromIntegral <$> eval n)
+
+solve1 :: M.Map RefId Exp -> Word16
+solve1 = evalState (eval $ Ref "a")
+
+solve2 :: M.Map RefId Exp -> Word16
+solve2 m = evalState (eval $ Ref "a") $ M.insert "b" (Val $ solve1 m) m
+
+main :: IO ()
+main = do
+  s <- readFile "Day07.txt"
+  case parseDefs s :: Either ParseError [Def] of
+    Left e   -> error $ show e
+    Right xs -> print . sequence [ solve1
+                                 , solve2
+                                 ] $ M.fromList xs
--- src/Main.hs
+++ src/Main.hs
@@ -3,23 +3,25 @@ module Main where
 import qualified Day01
 import qualified Day02
 import qualified Day03
-import qualified Day04
+-- import qualified Day04
 import qualified Day05
-import qualified Day06
+-- import qualified Day06
 import qualified Day06Perf
+import qualified Day07
 
 import Text.Printf (printf)
 
 main :: IO ()
 main = mapM_
     (\ (day, ioa) -> printf "Day%-10s: " day >> ioa)
-    [ ("01",     Day01.main)
-    , ("02",     Day02.main)
-    , ("03",     Day03.main)
-    , ("04 *",   putStrLn "[117946,3938038]")
-    , ("05",     Day05.main)
-    , ("06 *",   putStrLn "[400410,15343601]")
-    , ("06Perf", Day06Perf.main)
+    [  ("01",     Day01.main)
+     , ("02",     Day02.main)
+     , ("03",     Day03.main)
+     , ("04 *",   putStrLn "[117946,3938038]")
+     , ("05",     Day05.main)
+    --  , ("06",     Day06.main)
+     , ("06Perf", Day06Perf.main)
+     , ("07",     Day07.main)
     ]
     >> putStrLn ""
     >> putStrLn "[*] replaced with a constant to save time"
