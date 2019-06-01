1c5286ef318ab68d4eb92ae587a3d7833aea07ce
Oleksandr Shyshko <oshyshko@gmail.com>
2019-05-31 21:09:39 -0600

    Refactor Y15.D12

--- src/Y15/D12.hs
+++ src/Y15/D12.hs
@@ -2,36 +2,25 @@
 
 module Y15.D12 where
 
-import           Data.Aeson                 (Value (..), decode)
+import           Data.Aeson                 (Object, Value (..), decode)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.HashMap.Strict        (elems)
 import           Data.Maybe                 (fromJust, fromMaybe, maybeToList)
 import           Data.Scientific            (toBoundedInteger)
 
-parseJsonOrDie :: String -> Value
-parseJsonOrDie s = fromMaybe
-    (error $ "Could not parse JSON: " ++ s)
-    (decode (BL.pack s))
-
--- TODO refactor
-solve1 :: String -> Int
-solve1 =
+solve :: (Object -> Bool) -> String -> Int
+solve digObject =
     sum . nums . fromJust . decode . BL.pack
   where
     nums :: Value -> [Int]
     nums = \case
-        Object m -> concatMap nums m
+        Object m -> if digObject m then concatMap nums (elems m) else []
         Array v  -> concatMap nums v
         Number n -> maybeToList . toBoundedInteger $ n
         _        -> []
+
+solve1 :: String -> Int
+solve1 = solve $ const True
 
 solve2 :: String -> Int
-solve2 =
-    sum . nums . fromJust . decode . BL.pack
-  where
-    nums :: Value -> [Int]
-    nums = \case
-        Object m -> if "red" `elem` elems m then [] else concatMap nums (elems m)
-        Array v  -> concatMap nums v
-        Number n -> maybeToList . toBoundedInteger $ n
-        _        -> []
+solve2 = solve $ \m -> "red" `notElem` elems m
