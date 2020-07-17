0329fa48673d682d316260ce4f2527a5d8afac01
Oleksandr Shyshko <oshyshko@gmail.com>
2020-07-17 07:35:34 +0300

    Add ./scripts/auto-exec.sh

---
+++ scripts/auto-exec.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -xue
+
+# jump to project directory
+cd "$(dirname $0)/../"
+
+# taken from https://www.parsonsmatt.org/2018/05/19/ghcid_for_the_win.html
+ghcid -W \
+    --command "stack ghci adventofcode:lib adventofcode:adventofcode-exe" \
+    --test "Main.mainArgs [\"$@\"]"
