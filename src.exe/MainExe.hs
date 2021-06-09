ec8a23c84e7c3415a34e4ed8abaedfd7df34a0ee
Alex Shyshko <oshyshko@gmail.com>
2021-06-10 01:08:16 +0300

    Undo Y15.D19 in MainExe

--- src.exe/MainExe.hs
+++ src.exe/MainExe.hs
@@ -35,7 +35,6 @@ import qualified Y15.D15
 import qualified Y15.D16
 import qualified Y15.D17
 import qualified Y15.D18
-import qualified Y15.D19
 
 days :: Map String [String -> IO String]
 days = M.fromList $ join $(MainExeTH.solversFromImportsAndSources)
