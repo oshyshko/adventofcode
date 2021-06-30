730bd078c3ecb34dc50c0fd461a8828fcbf19b11
Alex Shyshko <oshyshko@gmail.com>
2021-06-30 17:32:16 +0300

    Update auto-test.sh + package.yaml

--- scripts/auto-test.sh
+++ scripts/auto-test.sh
@@ -7,5 +7,5 @@ cd "$(dirname $0)/../"
 
 # taken from https://www.parsonsmatt.org/2018/05/19/ghcid_for_the_win.html
 ghcid -W \
-    --command "stack ghci adventofcode:lib adventofcode:adventofcode-test" \
+    --command "stack ghci adventofcode:lib adventofcode:test:spec" \
     --test "Main.main $@"
