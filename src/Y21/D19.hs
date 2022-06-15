b68722131fddd00e21213ee7e29fee9c1e6ccb11
Alex Shyshko <oshyshko@gmail.com>
2022-06-15 17:57:43 +0300

    Refactor Y21.D19

--- src/Y21/D19.hs
+++ src/Y21/D19.hs
@@ -11,12 +11,12 @@ type Coord    = Int
 data XYZ      = XYZ Coord Coord Coord deriving (Eq, Ord, Show)
 
 instance Num XYZ where
-    (XYZ x y z) + (XYZ a b c) = XYZ (x + a) (y + b) (z + c)
-    (XYZ x y z) - (XYZ a b c) = XYZ (x - a) (y - b) (z - c)
-    (XYZ x y z) * (XYZ a b c) = XYZ (x * a) (y * b) (z * c)
-    abs           (XYZ x y z) = XYZ (abs x) (abs y) (abs z)
+    (XYZ x y z) + (XYZ a b c) = XYZ (x + a)    (y + b)    (z + c)
+    (XYZ x y z) - (XYZ a b c) = XYZ (x - a)    (y - b)    (z - c)
+    (XYZ x y z) * (XYZ a b c) = XYZ (x * a)    (y * b)    (z * c)
+    abs           (XYZ x y z) = XYZ (abs x)    (abs y)    (abs z)
     negate        (XYZ x y z) = XYZ (negate x) (negate y) (negate z)
-    signum        _           = error "Not implemented"
+    signum        (XYZ x y z) = XYZ (signum x) (signum y) (signum z)
     fromInteger x             = let v = fromIntegral x in XYZ v v v
 
 data Region = Region
@@ -53,36 +53,24 @@ rolls =
     , \(XYZ x y z) -> XYZ         z          y  (negate x)
     ]
 
-diffImprint :: [XYZ] -> [XYZ]
-diffImprint beacons =
-    [ a - b
-    | a <- beacons
-    , b <- beacons
-    , a /= b
-    ]
+diffs :: [XYZ] -> [XYZ]
+diffs xs = [ a - b | a <- xs, b <- xs, a /= b ]
 
-productImprint :: [XYZ] -> [Coord]
-productImprint beacons =
-    [ a - b & \(XYZ x y z) -> abs $ x * y * z
-    | a <- beacons
-    , b <- beacons
-    , a /= b
-    ]
+diffsProducts :: [XYZ] -> [Coord]
+diffsProducts xs = diffs xs <&> \(XYZ x y z) -> abs $ x * y * z
 
 intersectionCount :: Ord a => Set a -> [a] -> Int
-intersectionCount a b =
-    S.size a - S.size (foldl' (flip S.delete) a b)
+intersectionCount a b = S.size a - S.size (foldl' (flip S.delete) a b)
 
 -- > uniquePairs [1..4]
 -- [(1,2), (1,3), (1,4), (2,3), (2,4), (3,4)]
 uniquePairs :: [a] -> [(a,a)]
-uniquePairs = \case
-    []     -> []
-    (x:xs) -> ((x,) <$> xs) ++ uniquePairs xs
+uniquePairs []     = []
+uniquePairs (x:xs) = ((x,) <$> xs) <> uniquePairs xs
 
 findLinks :: [Region] ->  Map RegionId [RegionId]
 findLinks rs = rs
-    & fmap (\r -> (r, productImprint (beacons r)))
+    & fmap (\r -> (r, diffsProducts (beacons r)))
     & uniquePairs
     & concatMap (\((arid, api), (brid, bpi)) ->
         let c = intersectionCount (S.fromList api) bpi
@@ -110,9 +98,9 @@ findMergeOrder start rid2rids =
 
 findRegionFix :: [XYZ] -> [XYZ] -> (XYZ -> XYZ)
 findRegionFix as bs =
-    let sa     = S.fromList $ diffImprint as
+    let sa     = S.fromList $ diffs as
         orient =
-            [ (intersectionCount sa (diffImprint $ f <$> bs), f)
+            [ (intersectionCount sa (diffs $ f <$> bs), f)
             | d <- directions
             , r <- rolls
             , let f = d . r
