c731a1ad524baecf6d17858561d3b1708e3365bc
Alex Shyshko <oshyshko@gmail.com>
2022-06-30 06:00:06 +0300

    Add test filter

--- scripts/auto-test.sh
+++ scripts/auto-test.sh
@@ -5,7 +5,11 @@ set -xue
 # jump to project directory
 cd "$(dirname $0)/../"
 
+# Example:
+# ./scripts/auto-test.sh '--match "/Y21.D22/"'
+
 # taken from https://www.parsonsmatt.org/2018/05/19/ghcid_for_the_win.html
 ghcid -W \
     --command "stack ghci adventofcode:lib adventofcode:test:spec" \
-    --test "Main.main $@"
+    --test "Main.main" \
+    --setup ":set args $@"
