309244814b9aedd7963e802ca304f6ee133cbda7
Alex Shyshko <oshyshko@gmail.com>
2022-12-21 02:55:01 +0200

    Extract Pathfinder from Y21.D15

---
+++ src/Pathfinder.hs
@@ -0,0 +1,85 @@
+module Pathfinder where
+
+import           Control.Monad.ST            (ST, runST)
+import qualified Data.IntMap.Strict          as M
+import qualified Data.IntPSQ                 as Q
+import qualified Data.STRef                  as S
+import qualified Data.Vector.Unboxed.Mutable as VUM
+
+import           Imports
+import           Util
+
+-- p - point
+-- v - value
+-- s - score
+
+{-# INLINE[1] minScoreIntMap #-} -- TODO remove?
+minScoreIntMap :: (p ~ Int, Bounded s, Ord s, Num s)
+    => (p -> [p]) -> (p -> v) -> (v -> s)                               -- board
+    -> p -> p -> s                                                      -- start goal
+minScoreIntMap = minScore M.empty (M.findWithDefault maxBound) M.insert
+
+{-# INLINE[1] minScore #-} -- TODO remove?
+minScore :: (p ~ Int, Bounded s, Ord s, Num s)
+    => ss -> (p -> ss -> s) -> (p -> s -> ss -> ss)                     -- score storage
+    -> (p -> [p]) -> (p -> v) -> (v -> s)                               -- board
+    -> p -> p -> s                                                      -- start goal
+minScore ssEmpty getScore setScore neighbors at score start goal =
+    fix2
+        (Q.singleton start maxBound ())                                 -- open
+        (setScore start 0 ssEmpty)                                      -- xy2score
+        \loop open ss ->
+            Q.alterMin (,Nothing) open & \case
+                (Nothing, _) -> error "No path"                         -- exhausted ?
+                (Just (current, _, _), openEjected) ->
+                    let currentScore = getScore current ss
+                    in if current == goal                               -- goal reached?
+                        then currentScore
+                        else
+                              neighbors current                         -- for each neighbor
+                            & foldl'                                    -- apply collected changes
+                                (\(openAcc,ssAcc) nXyi ->
+                                    let nRisk     = at nXyi
+                                        nScore    = score nRisk  + currentScore
+                                        nScoreOld = getScore nXyi ss
+                                    in if nScore < nScoreOld
+                                        then ( Q.insert nXyi nScore () openAcc
+                                             , setScore nXyi nScore ssAcc
+                                             )
+                                        else (openAcc,ssAcc)
+                                    )
+                                (openEjected,ss)
+                            & uncurry loop
+
+{-# INLINE[1] minScoreMVector #-}
+minScoreMVector :: (p ~ Int, Bounded s, Ord s, Num s, VUM.Unbox s)
+    => Int                                                              -- score storage (size)
+    -> (p -> [p]) -> (p -> v) -> (v -> s)                               -- board
+    -> p -> p -> s                                                      -- start goal
+minScoreMVector scoreStorageSize neighbors at score start goal =
+    runST $ do
+        let mkStoreStorage    = VUM.replicate scoreStorageSize maxBound
+            readScore         = flip $ VUM.read @(ST _)
+            writeScore p s ss = VUM.write ss p s
+
+        open <- S.newSTRef $ Q.singleton start maxBound ()
+        ss <- mkStoreStorage
+        writeScore start 0 ss
+
+        fix \loop -> do
+            S.readSTRef open <&> Q.alterMin (,Nothing) >>= \case
+                (Nothing, _) -> error "No path"                         -- exhausted ?
+                (Just (current, _, _), openEjected) -> do
+                    currentScore <- readScore current ss
+                    if current == goal                                  -- goal reached?
+                        then pure currentScore
+                        else do
+                            S.writeSTRef open openEjected
+                            forM_ (neighbors current) \nXyi -> do       -- for each neighbor
+                                let nRisk  = at nXyi
+                                    nScore = score nRisk + currentScore
+                                nScoreOld <- readScore nXyi ss
+                                when (nScore < nScoreOld) $ do          -- apply changes
+                                    S.modifySTRef' open (Q.insert nXyi nScore ())
+                                    writeScore nXyi nScore ss
+                            loop
--- src/Y21/D15.hs
+++ src/Y21/D15.hs
@@ -1,16 +1,11 @@
 module Y21.D15 where
 
-import           Control.Monad.ST            (runST)
-import qualified Data.IntMap.Strict          as M
-import qualified Data.IntPSQ                 as Q
-import qualified Data.STRef                  as S
-import qualified Data.Vector.Generic         as VG
-import qualified Data.Vector.Unboxed.Mutable as VUM
+import qualified Data.Vector.Generic as VG
 
 import           Imports
-import           Util
-import qualified Vec2                        as V
-import           Vec2                        (Vec2 (..))
+import qualified Pathfinder
+import qualified Vec2                as V
+import           Vec2                (Vec2 (..))
 import           XY
 
 type Risk   = Word8
@@ -22,20 +17,6 @@ type Score  = Word16
 parse :: String -> Vec2 Risk
 parse = V.fromList . fmap (fmap $ fromIntegral . digitToInt) . lines
 
-tileTimes :: Integral v => Int -> Vec2 v -> Vec2 v
-tileTimes t Vec2{wh,vec} =
-    let (XY w h)       = wh
-        wht@(XY wt ht) = scale wh t
-    in Vec2
-        { wh  = wht
-        , vec = VG.fromList $ do
-            y <- [0..(ht - 1)]
-            x <- [0..(wt - 1)]
-            pure $ (vec VG.! xy2i wh (XY (x `mod` w) (y `mod` h)))
-                & (+) (fromIntegral $ x `div` w + y `div` h)
-                & pred & (`mod` 9) & succ
-        }
-
 neighbors :: WH -> XY -> [XYI]
 neighbors wh@(XY w h) xy =
     [ xy2i wh nXy
@@ -44,67 +25,19 @@ neighbors wh@(XY w h) xy =
     , nx >= 0 && ny >= 0 && nx < w && ny < h                            -- keep traversable ones only
     ]
 
-minScore :: XYI -> XYI -> Vec2 Risk -> Score
-minScore start goal Vec2{wh,vec} =
-    fix2
-        (Q.singleton start maxBound ())  -- open
-        (M.singleton start 0)            -- xy2score
-        \loop open xyi2score ->
-            Q.alterMin (,Nothing) open & \case
-                (Nothing, _) -> error "No path"                         -- exghausted ?
-                (Just (current, _, _), openEjected) ->
-                    let currentScore = xyi2score M.! current
-                    in if current == goal                               -- goal reached?
-                        then currentScore
-                        else
-                              neighbors wh (i2xy wh current)            -- for each neighbor
-                            & foldl'                                    -- apply collected changes
-                                (\(openAcc,xyi2scoreAcc) nXyi ->
-                                    let nRisk     = vec VG.! nXyi
-                                        nScore    = fromIntegral nRisk + currentScore
-                                        nScoreOld = M.findWithDefault maxBound nXyi xyi2score
-                                    in if nScore < nScoreOld
-                                        then ( Q.insert nXyi nScore () openAcc
-                                             , M.insert nXyi nScore xyi2scoreAcc
-                                             )
-                                        else (openAcc,xyi2scoreAcc)
-                                    )
-                                (openEjected,xyi2score)
-                            & uncurry loop
+at :: Vec2 Risk -> XYI -> Risk
+at Vec2{vec} = (vec VG.!)
 
-minScoreMU :: XYI -> XYI -> Vec2 Risk -> Score
-minScoreMU start goal Vec2{wh,vec} = runST $ do
-    open      <- S.newSTRef $ Q.singleton start (maxBound::Score) ()
-    xyi2score <- VUM.replicate (VG.length vec) (maxBound::Score)
+atTiled :: Vec2 Risk -> WH -> XYI -> Risk
+atTiled Vec2{wh,vec} wht i =
+    let (XY w h) = wh
+        (XY x y) = i2xy wht i
+    in    vec VG.! xy2i wh (XY (x `mod` w) (y `mod` h))                 -- risk
+        & (+) (fromIntegral $ x `div` w + y `div` h)                    -- add tile value
+        & pred & (`mod` 9) & succ                                       -- wrap around
 
-    VUM.write xyi2score start 0
-
-    fix \loop -> do
-        S.readSTRef open <&> Q.alterMin (,Nothing) >>= \case
-            (Nothing, _) -> error "No path"                             -- exghausted ?
-            (Just (current, _, _), openEjected) -> do
-                S.writeSTRef open openEjected
-                currentScore <- VUM.read xyi2score current
-                if current == goal                                      -- goal reached?
-                    then return currentScore
-                    else do                                             -- for each neighbor
-                        forM_ (neighbors wh (i2xy wh current)) \nXyi -> do
-                            let nRisk  = vec VG.! nXyi
-                                nScore = fromIntegral nRisk + currentScore
-                            nScoreOld <- VUM.read xyi2score nXyi
-                            when (nScore < nScoreOld) $ do              -- apply changes
-                                S.modifySTRef' open (Q.insert nXyi nScore ())
-                                VUM.write xyi2score nXyi nScore
-                        loop
-
-solve :: (XYI -> XYI -> Vec2 Risk -> Score) -> Vec2 Risk -> Int
-solve minScoreF visit@Vec2{wh} =
-    let start = 0
-        goal  = xy2i wh (wh - 1)
-    in fromIntegral $ minScoreF start goal visit
-
-solve1, solve2, solve1MU, solve2MU :: String -> Int
-solve1   = solve minScore                 . parse
-solve2   = solve minScore   . tileTimes 5 . parse
-solve1MU = solve minScoreMU               . parse
-solve2MU = solve minScoreMU . tileTimes 5 . parse
+solve1, solve2, solve1IM, solve2IM :: String -> Int
+solve1   = (\v@Vec2{wh,vec} ->                           Pathfinder.minScoreMVector (        VG.length vec) (neighbors wh  . i2xy wh)  (at v)          fromIntegral 0 (        VG.length vec - 1)) <$> parse
+solve2   = (\v@Vec2{wh,vec} -> let wht = wh `scale` 5 in Pathfinder.minScoreMVector (5 * 5 * VG.length vec) (neighbors wht . i2xy wht) (atTiled v wht) fromIntegral 0 (5 * 5 * VG.length vec - 1)) <$> parse
+solve1IM = (\v@Vec2{wh,vec} ->                           Pathfinder.minScoreIntMap                          (neighbors wh  . i2xy wh)  (at v)          fromIntegral 0 (        VG.length vec - 1)) <$> parse
+solve2IM = (\v@Vec2{wh,vec} -> let wht = wh `scale` 5 in Pathfinder.minScoreIntMap                          (neighbors wht . i2xy wht) (atTiled v wht) fromIntegral 0 (5 * 5 * VG.length vec - 1)) <$> parse
