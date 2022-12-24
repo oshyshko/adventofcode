3e1fa1e24a8d3788c0e73a8a86355fb23244f7fd
Alex Shyshko <oshyshko@gmail.com>
2022-12-24 18:05:02 +0200

    Cleanup

--- src/Y22/D14.hs
+++ src/Y22/D14.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use uncurry" #-}
 module Y22.D14 where
 
 import qualified Data.Map.Strict as M
@@ -10,65 +8,57 @@ import           XY
 
 type Cave = Map XY Bool
 
-parseCave :: String -> Cave
-parseCave =
-      foldl' (\m xy -> M.insert xy True m) M.empty
-    . concatMap (\(a,b) -> line2dots a b)
-    . concatMap (divvy2 1)
-    . parseOrDie traces
+-- 498,4 -> 498,6 -> 496,6
+-- 503,4 -> 502,4 -> 502,9 -> 494,9
+traces :: Parser [[XY]]
+traces =
+    trace' `endBy` eol
   where
-    -- 498,4 -> 498,6 -> 496,6
-    -- 503,4 -> 502,4 -> 502,9 -> 494,9
-    traces :: Parser [[XY]]
-    traces =
-        trace' `endBy` eol
-      where
-        trace' = xy `sepBy` padded (string "->")
-        xy = XY <$> natural <* char ',' <*> natural
+    trace' = xy `sepBy` padded (string "->")
+    xy = XY <$> natural <* char ',' <*> natural
 
+fillCave
+    :: (Cave -> XY -> Bool)                         -- stopOnFallout
+    -> (Cave -> XY -> Bool)                         -- occupied
+    -> Cave                                         -- cave
+    -> XY                                           -- startXy
+    -> Either Cave Cave                             -- left = infinitely dropping, right = nowhere to place
+fillCave stopOnFallout occupied cave startXy =
+    fix2 startXy (Right cave) \loop xy e ->
+        e >>= \m ->                                 -- shortcut on Left Cave, continue on Right Cave
+            if | stopOnFallout m xy -> Left m       -- out of the board => stop
+               | occupied m xy      -> Right m      -- occupied => fallback
+               | otherwise ->
+                     fmap (M.insert xy True)        -- map remaining sand blocks (at rest)
+                   . loop (xy + XY 1 1)             -- ... down-right
+                   . loop (xy + XY (-1) 1)          -- ... down-left
+                   . loop (xy + XY 0 1) $ Right m   -- ... down (start with it)
+
+solve :: (Int -> Cave -> XY -> Bool) -> (Int -> Cave -> XY -> Bool) -> String -> Int
+solve stopOnFallout occupied s =
+    let cave           = parseOrDie traces s
+                            & concatMap (divvy2 1)
+                            & concatMap (uncurry line2dots)
+                            & foldl' (\m xy -> M.insert xy True m) M.empty
+        startXy        = XY 500 0
+        maxY           = foldl' max minBound . fmap getY $ startXy : M.keys cave
+        caveFilled     = fillCave (stopOnFallout maxY) (occupied maxY) cave startXy
+        caveFilledSize = M.size case caveFilled of Left n -> n; Right n -> n
+    in caveFilledSize - M.size cave
+  where
     line2dots ::  XY -> XY -> [XY]
     line2dots a b =
-        let dx = signum $ b - a                             -- TODO assert |dx| = 1
+        let dx = signum $ b - a                     -- TODO assert |dx| = 1
             n  = abs (b - a) & \(XY x y) -> x + y
         in take (1+n) (iterate (+ dx) a)
 
-fillCave
-    :: (XY -> XY -> Cave -> XY -> Bool)
-    -> (XY -> XY -> Cave -> XY -> Bool)
-    -> Cave
-    -> XY
-    -> Either Cave Cave
-fillCave stop occupied cave start =
-    let minXy = foldl' (xyBiMap min) maxBound $ start : M.keys cave
-        maxXy = foldl' (xyBiMap max) minBound $ start : M.keys cave
-    in fix2 start (Right cave) \loop xy e ->
-        e >>= \m ->                                         -- shortcut on Left Cave, continue on Right Cave
-            if | stop minXy maxXy m xy     -> Left m        -- out of the board => stop
-               | occupied minXy maxXy m xy -> Right m       -- occupied => fallback
-               | otherwise ->
-                       fmap (M.insert xy True)              -- map remaining sand blocks (at rest)
-                   . loop (xy + XY 1 1)                     -- ... down-right
-                   . loop (xy + XY (-1) 1)                  -- ... down-left
-                   . loop (xy + XY 0 1) $ Right m           -- ... down (start with it)
-
-solve
-    :: (XY -> XY -> Cave -> XY -> Bool)
-    -> (XY -> XY -> Cave -> XY -> Bool)
-    -> String -> Int
-solve stop occupied s =
-    let cave       = parseCave s
-        caveFilled = fillCave stop occupied cave (XY 500 0)
-    in subtract (M.size cave) case caveFilled of
-        Left n  -> M.size n                                 -- sand blocks falling out of the map
-        Right n -> M.size n                                 -- no space for sand blocks to be added
-
 solve1 :: String -> Int
 solve1 = solve
-    (\_ (XY _ maxY) _ (XY _ y) -> y > maxY)                 -- stop     = when 1st block leaves the cave
-    (\_ _ m xy -> xy `M.member` m)                          -- occupied = obstructed
+    (\maxY _ (XY _ y) -> y > maxY)                  -- stopOnFallout = when falling through the floor
+    (\_ m xy -> xy `M.member` m)                    -- occupied      = obstructed
 
 solve2 :: String -> Int
 solve2 = solve
-    (\_ _ _ _ -> False)                                     -- stop     = never
-    (\_ (XY _ maxY) m xy@(XY _ y) ->                        -- occupied = obstructed or reached the floor
+    (\_ _ _ -> False)                               -- stopOnFallout = no
+    (\maxY m xy@(XY _ y) ->                         -- occupied      = obstructed or reached the floor
         xy `M.member` m || y >= maxY + 2)
