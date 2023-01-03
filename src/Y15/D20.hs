4267b6fe8ccc88ab7c6503309e6b9b045f0653e9
Alex Shyshko <oshyshko@gmail.com>
2023-01-03 13:51:32 +0200

    Refactor Y15.D20

--- src/Y15/D20.hs
+++ src/Y15/D20.hs
@@ -1,40 +1,24 @@
 module Y15.D20 where
 
-import Math.NumberTheory.Primes (factorise, unPrime)
+import           Math.NumberTheory.Primes (factorise, unPrime)
 
-{-# INLINE divisorSum #-}
-divisorSum :: Int -> Int
-divisorSum i = product
-    [ s
-    | (unPrime -> p, n) <- factorise i
-    , let s = sum [ p ^ x | x <- [0..n] ]
-    ]
-
-{-# INLINE divisors #-}
-divisors :: Int -> [Int]
-divisors =
-    go [1] . factorise
-  where
-    go xs [] = xs
-    go xs ((unPrime -> p, n):rest) =
-        go [ x * p^nn | x <- xs, nn <- [0..n] ] rest
+findFirstLE :: (Int -> Int) -> String -> Int
+findFirstLE f s = head [ i | i <- [1..], read s <= f i ]
 
 solve1 :: String -> Int
-solve1 s =
-    let minPresents = read s
-    in head
-        [ house
-        | house <- [1..]
-        , let presents = divisorSum house * 10
-        , minPresents <= presents
+solve1 = findFirstLE \i -> 10 * divisorSum i
+  where
+     divisorSum i = product
+        [ sum [ p ^ x | x <- [0..n] ]
+        | (unPrime -> p, n) <- factorise i
         ]
 
 solve2 :: String -> Int
-solve2 s =
-    let minPresents = read s
-    in head
-        [ house
-        | house <- [1..]
-        , let presents = sum (filter (house `div` 50 <) $ divisors house) * 11
-        , minPresents <= presents
+solve2 = findFirstLE \i -> 11 * sum (filter (i `div` 50 <) $ divisors i)
+  where
+    divisors = foldr f [1] . factorise
+    f (unPrime -> p, n) ys =
+        [ y * p ^ x
+        | x <- [0..n]
+        , y <- ys
         ]
